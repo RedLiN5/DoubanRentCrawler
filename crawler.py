f340224724bdc3e5ab5a740f84dc6b6f89aa112b
Leslie_Yan <leslieyan0621@gmail.com>
2017-04-02 17:39:28 +0800

    [Feature] Add cookies

--- crawler.py
+++ crawler.py
@@ -47,7 +47,9 @@ def _crawler(urlfront, max_page, keywords):
         url = urlfront + 'discussion?start=' + str(page_index)
         headers = {'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:51.0) "+
                                  "Gecko/20100101 Firefox/51.0"}
+        cookie = {'Cookie':'bid=yHcxtL5NO2c; ct=y; ap=1; ps=y; ll="108296"; __utmt=1; _ga=GA1.2.1545850150.1470297820; _gat_UA-7019765-1=1; ue="4372125@QQ.COM"; dbcl2="63247631:RTx25cp0Rdk"; ck=MIk5; push_noty_num=0; push_doumail_num=0; _pk_id.100001.8cb4=0c73410bb3c4761a.1470297777.4.1491123584.1491027175.; _pk_ses.100001.8cb4=*; __utma=30149280.1545850150.1470297820.1491027175.1491123542.4; __utmb=30149280.3.10.1491123542; __utmc=30149280; __utmz=30149280.1490949855.2.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=30149280.6324; _vwo_uuid_v2=15011FAE62D39D35F3E1381D306D622D|e5c4760540aeda3ad018f44b0537c92a'}
         page = requests.get(url, headers=headers,
+                            cookies=cookie,
                             timeout=(3.3, 5))
         soup = bs4.BeautifulSoup(page.content, 'lxml')
         items = soup.findAll('td', 'title')
